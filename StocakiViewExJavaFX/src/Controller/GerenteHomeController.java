00a78dcd4735d57c24e6d9336c475680f67bf731
Baruffi <42071700+Baruffi@users.noreply.github.com>
2019-04-13 22:48:26 -0300

    Add files via upload

---
+++ StocakiViewExJavaFX/src/Controller/GerenteHomeController.java
@@ -0,0 +1,11 @@
+package Controller;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+public class GerenteHomeController {
+
+    public GerenteHomeController() { }
+}
