ee63478b596707a3f1a3e4eabdf0ef8c145620a5
Raphael Baruffi <raphabaruffi@gmail.com>
2019-05-19 20:57:50 -0300

    inicio do trabalho nas DAOs

--- Connection/src/main/java/DAO/DataConnection.java
+++ Connection/src/main/java/DAO/DataConnection.java
@@ -2,14 +2,14 @@ package DAO;
 
 import java.sql.*;
 
-public class DataConnection {
+class DataConnection {
 
     private static final String url = "jdbc:mysql://localhost:3306/stocakiBD?useTimezone=true&serverTimezone=UTC",
                                 user = "root",
                                 password = "root",
                                 driver = "com.mysql.cj.jdbc.Driver";
 
-    public Connection getConnection() {
+    Connection getConnection() {
         Connection con = null;
         try{
             Class.forName(driver);
@@ -21,7 +21,7 @@ public class DataConnection {
             return con;
         }
     }
-    public void closeConnection(PreparedStatement ps, ResultSet rs, Connection con) {
+    void closeConnection(PreparedStatement ps, ResultSet rs, Connection con) {
         try{
             ps.close();
             rs.close();
